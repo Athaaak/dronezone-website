71d39100a8d71a0bb5b0f61fae04a5e0b186255d
farreldavaa <farrel.dava@binus.ac.id>
2023-05-19 11:41:32 +0700

    landing page

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,14 @@
+@extends('template.template')
+
+@section('title')
+    Dronezone
+@endsection
+
+@section('content')
+    <div class="d-flex justify-content-center">
+        <video controls height="auto" width="auto">
+            <source src="img/videoplayback.mp4"/>
+       </video>
+    </div>
+
+@endsection
---
+++ resources/views/template/navbar.blade.php
@@ -0,0 +1,46 @@
+<div id="menuHolder">
+    <div role="navigation" class="sticky-top border-bottom border-top" id="mainNavigation">
+      <div class="flexMain">
+        <div class="flex2">
+          <button class="whiteLink siteLink" style="border-right:1px solid #eaeaea" onclick="menuToggle()"><i class="fas fa-clock"></i></i> MENU</button>
+        </div>
+        <div class="flex3 text-center" id="siteBrand">
+          DRONEZONESIA
+        </div>
+
+        <div class="flex2 text-end d-block d-md-none">
+          <button class="whiteLink siteLink"><i class="fas fa-search"></i></button>
+        </div>
+
+        <div class="flex2 text-end d-none d-md-block">
+          <button class="whiteLink siteLink">Register</button>
+          <button class="whiteLink siteLink">Login</button>
+        </div>
+      </div>
+    </div>
+
+    <div id="menuDrawer">
+      <div class="p-4 border-bottom">
+        <div class='row'>
+          <div class="col">
+            <select class="noStyle">
+              <option value="english">English</option>
+              <option value="spanish">Indonesia</option>
+            </select>
+          </div>
+          <div class="col text-end ">
+            <i class="fas fa-times" role="btn" onclick="menuToggle()"></i>
+          </div>
+        </div>
+      </div>
+      <div>
+        <a href="#" class="nav-menu-item"><i class="fas fa-home me-3"></i>Home</a>
+        <a href="#" class="nav-menu-item"><i class="fab fa-product-hunt me-3"></i>Products</a>
+        <a href="#" class="nav-menu-item"><i class="fas fa-search me-3"></i>Explore</a>
+        <a href="#" class="nav-menu-item"><i class="fas fa-wrench me-3"></i>Services</a>
+        <a href="#" class="nav-menu-item"><i class="fas fa-dollar-sign me-3"></i>Pricing</a>
+        <a href="#" class="nav-menu-item"><i class="fas fa-file-alt me-3"></i>Blog</a>
+        <a href="#" class="nav-menu-item"><i class="fas fa-building me-3"></i>About Us</a>
+      </div>
+    </div>
+  </div>
