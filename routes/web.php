e8ef5e3d298e1959df6ef3c9af6b9e58789e7e93
farreldavaa <farrel.dava@binus.ac.id>
2023-05-20 07:48:27 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\HomepageController;
-=======
->>>>>>> f1f65041cf3cc36a808bc4359849db1c6d413e63
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +14,9 @@ use App\Http\Controllers\HomepageController;
 |
 */
 
-<<<<<<< HEAD
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
 Route::get('/',[HomepageController::class, 'homepage']);
 
-=======
-Route::get('/', function () {
-    return view('welcome');
-});
->>>>>>> f1f65041cf3cc36a808bc4359849db1c6d413e63
