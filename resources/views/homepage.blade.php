1c1129d1d8f942c1711a1ab9b9cdc28c8b634374
Athallah Putra <atha.omniappsolutions@gmail.com>
2023-06-18 18:09:42 +0700

    Added Header text and fixed video positioning

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -10,11 +10,16 @@
     </head>
     <div class="landing-page">
         <div class="video-box">
-
+            <div class="video-container">
+            <video class="video" autoplay muted playsinline>
+                    <source src="img/videoplayback.mp4"/>
+                </video>
+                <div class="text-container">
+                <label class="header">Sewa Drone <br>Jadi lebih mudah<br> Di Surabaya</label>
+                <h5 class="description">Melalui DRONEZONE, anda dapat dengan mudah menelusuri berbagai penyedia sewa drone, membandingkan harga, spesifikasi, dan ulasan pengguna di Surabaya. Kami berkomitmen untuk menyediakan informasi yang jelas dan akurat sehingga anda dapat membuat keputusan yang tepat sesuai dengan kebutuhan anda.</h5>
+            </div>
+            </div>
         </div>
-        <video class="video" controls height="auto" width="auto">
-            <source src="img/videoplayback.mp4"/>
-        </video>
     </div>
 
 @endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,4 @@
 let mix = require('laravel-mix');
 
 mix.sass('resources/sass/login.scss', 'public/css');
+mix.browserSync('127.0.0.1:8000');
